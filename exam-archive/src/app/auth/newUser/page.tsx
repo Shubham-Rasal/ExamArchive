3ed6cd529c3a6ae738cdbcab79a5033bfc6123c0
bismay <bismaypurkayastha5467@gmail.com>
2023-12-19 19:54:56 +0530

    routes and pages for authentication setup

---
+++ exam-archive/src/app/auth/newUser/page.tsx
@@ -0,0 +1,153 @@
+"use client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const registerFormSchema = z
+  .object({
+    email: z
+      .string()
+      .trim()
+      .toLowerCase()
+      .email({ message: "Invalid email address" }),
+    username: z
+      .string()
+      .trim()
+      .min(5)
+      .max(10, { message: "Username must be 10 or fewer characters long" }),
+    password: z
+      .string()
+      .trim()
+      .min(6, { message: "Minimum length of the password must be 6" })
+      .max(8, { message: "Maximum length of the password must be 8" })
+      .refine(
+        (password) => {
+          const digitRegex = /\d/;
+          const symbolRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
+          return digitRegex.test(password) && symbolRegex.test(password);
+        },
+        { message: "Password must contain atleast one digit and one symbol" }
+      ),
+  })
+  .required();
+
+export type TRegister = z.infer<typeof registerFormSchema>;
+
+export default function Register() {
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const formProps = useForm<TRegister>({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = async (values: TRegister) => {
+    setIsSubmitting(true);
+
+    const res = await fetch("/api/auth/newUser", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      const { message } = (await res.json()) as IResponse;
+      setError(message);
+      return;
+    }
+
+    setIsSubmitting(false);
+    setError(null);
+    formProps.reset();
+
+    router.push("/dashboard");
+  };
+
+  return (
+    <Form {...formProps}>
+      <form onSubmit={formProps.handleSubmit(handleSubmit)}>
+        <FormField
+          control={formProps.control}
+          name="username"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="Enter your username"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          control={formProps.control}
+          name="email"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Enter your email address"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          control={formProps.control}
+          name="password"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        {error !== null && <div>{error}</div>}
+        <Button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Registering..." : "Register"}
+        </Button>
+        <Link href={"/auth/signIn"}>
+          Already have an account? <span>Login</span>
+        </Link>
+      </form>
+    </Form>
+  );
+}
---
+++ exam-archive/src/app/auth/signIn/page.tsx
@@ -0,0 +1,123 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const loginFormSchema = z
+  .object({
+    username: z.string().trim(),
+    password: z
+      .string()
+      .trim()
+      .min(6, { message: "Minimum length of the password must be 6" })
+      .max(8, { message: "Maximum length of the password must be 8" })
+      .refine(
+        (password) => {
+          const digitRegex = /\d/;
+          const symbolRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/;
+          return digitRegex.test(password) && symbolRegex.test(password);
+        },
+        { message: "Password must contain atleast one digit and one symbol" }
+      ),
+  })
+  .required();
+
+export type TLogin = z.infer<typeof loginFormSchema>;
+
+export default function Login() {
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const formProps = useForm<TLogin>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: { username: "", password: "" },
+  });
+
+  const handleSubmit = async (values: TLogin) => {
+    setIsSubmitting(true);
+
+    const res = await fetch("/api/auth/signIn", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      const { message } = (await res.json()) as IResponse;
+      setError(message);
+      return;
+    }
+
+    setIsSubmitting(false);
+    setError(null);
+    formProps.reset();
+
+    router.push("/dashboard");
+  };
+
+  return (
+    <Form {...formProps}>
+      <form onSubmit={formProps.handleSubmit(handleSubmit)}>
+        <FormField
+          control={formProps.control}
+          name="username"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="text"
+                    placeholder="Enter your username or email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        <FormField
+          control={formProps.control}
+          name="password"
+          render={({ field }) => {
+            return (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            );
+          }}
+        />
+        {error !== null && <div>{error}</div>}
+        <Button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Verifying..." : "Login"}
+        </Button>
+        <Link href={"/auth/newUser"}>
+          Don't have an account? <span>Register</span>
+        </Link>
+      </form>
+    </Form>
+  );
+}
