595b9f6237674c81eed2c5ca8e387a004d093a78
bismay <bismaypurkayastha5467@gmail.com>
2023-12-19 19:51:26 +0530

    constants for status codes and other generic constants added

---
+++ exam-archive/src/constants/constants.ts
@@ -0,0 +1,5 @@
+export const MONGO_WRITE_QUERY_TIMEOUT = 15000;
+export const MONGO_READ_QUERY_TIMEOUT = 10000;
+export const AUTH_TOKEN: string = "auth-token";
+export const JWT_MAX_AGE = "24h";
+export const COOKIES_TTL = 60 * 60 * 24;
