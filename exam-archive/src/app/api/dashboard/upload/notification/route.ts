39dffb00c0c7032c768e528b117bfd3d2f90d61f
bismay <bismaypurkayastha5467@gmail.com>
2024-01-07 01:47:52 +0530

    required fields specified.

--- exam-archive/src/app/api/dashboard/upload/notification/route.ts
+++ exam-archive/src/app/api/dashboard/upload/notification/route.ts
@@ -1,8 +1,13 @@
 import { SUCCESS_CODES } from "@/constants/statuscode";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {
-  console.log("test", request.body);
+export async function POST(request: any) {
+  console.log(
+    "test",
+    request.body.secure_url,
+    request.body.url,
+    request.body.public_id
+  );
   return NextResponse.json(
     { message: "Everything seems good" },
     { status: SUCCESS_CODES.OK }
