e53bf7239723a02d023452cd6058f16e8e713990
bismay <bismaypurkayastha5467@gmail.com>
2024-01-07 14:28:06 +0530

    more constants added

--- exam-archive/src/constants/constants.ts
+++ exam-archive/src/constants/constants.ts
@@ -8,3 +8,43 @@ export const COOKIES_TTL = 60 * 60 * 24;
 export const ACTION = Object.freeze({ EMAIL: "email", RESET: "reset" });
 
 export const RESET_LINK_EXP_TIME = "48h";
+
+// TODO : add more exams
+
+export const EXAM_TYPES = {
+  INSTITUTIONAL: {
+    MIDSEM: "Midsem",
+    ENDSEM: "Endsem",
+    "QUIZ-I": "Quiz I",
+    "QUIZ-2": "Quiz II",
+    "PRACTICAL-I": "Practical I",
+    "PRACTICAL-II": "Practical II",
+  },
+};
+
+export const RATING_TYPE = Object.freeze({
+  HELPFUL: "helpful",
+  STANDARD: "standard",
+  RELEVANCE: "relevance",
+});
+
+export const SEMESTER = Object.freeze({
+  I: "Semester I",
+  II: "Semester II",
+  III: "Semester III",
+  IV: "Semester IV",
+  V: "Semester V",
+  VI: "Semester VI",
+  VII: "Semester VII",
+  VIII: "Semester VIII",
+});
+
+// TODO : add more branches
+
+export const BRANCH = Object.freeze({
+  CSE: "Computer Science and Engineering",
+});
+
+export const ALLOWED_FILE_FORMATS = ["jpg", "png", "pdf"] as const;
+export const TRANSFORMED_FORMAT = "pdf";
+export const MAX_FILE_SIZE = 200000;
