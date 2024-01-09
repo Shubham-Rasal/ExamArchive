bd98b76538079f1ef614aa192954ae6cc1e9c3ab
bismay <bismaypurkayastha5467@gmail.com>
2024-01-09 14:10:07 +0530

    debugging

--- exam-archive/src/app/api/dashboard/upload/notification/route.ts
+++ exam-archive/src/app/api/dashboard/upload/notification/route.ts
@@ -10,30 +10,34 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   const req = await request.json();
+  console.log(req);
 
-  const { public_id, secure_url, original_filename } = req as {
-    public_id?: string;
-    secure_url?: string;
-    original_filename?: string;
-  };
+  // const { public_id, secure_url } = req as {
+  //   public_id?: string;
+  //   secure_url?: string;
+  // };
 
-  if (!(public_id && secure_url && original_filename))
-    return NextResponse.json(
-      { message: "Couldn't update doc URL" },
-      { status: ERROR_CODES["BAD REQUEST"] }
-    );
+  // if (!(public_id && secure_url))
+  //   return NextResponse.json(
+  //     { message: "Couldn't update doc URL" },
+  //     { status: ERROR_CODES["BAD REQUEST"] }
+  //   );
+
+  // const sanitizedFileName = public_id.split("/").slice(-1);
+
+  // console.log("public_id: ", public_id, "filename: ", sanitizedFileName);
 
   try {
-    await connectDB();
+    // await connectDB();
 
-    await Question.findOneAndUpdate(
-      { "file.filename": original_filename },
-      { "file.public_id": public_id, "file.url": secure_url },
-      { upsert: false, new: true }
-    )
-      .maxTimeMS(MONGO_WRITE_QUERY_TIMEOUT)
-      .lean()
-      .exec();
+    // await Question.findOneAndUpdate(
+    //   { "file.filename": sanitizedFileName },
+    //   { "file.public_id": public_id, "file.url": secure_url },
+    //   { upsert: false, new: true }
+    // )
+    //   .maxTimeMS(MONGO_WRITE_QUERY_TIMEOUT)
+    //   .lean()
+    //   .exec();
 
     return NextResponse.json(
       { message: "Doc URL was updated successfully" },
