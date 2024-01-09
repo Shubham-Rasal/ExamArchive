04fb28adc29f2dba9505ad7d8639204a69e109d2
bismay <bismaypurkayastha5467@gmail.com>
2024-01-09 15:20:44 +0530

    resolved merge conflict

--- exam-archive/src/helpers/cloudinary.ts
+++ exam-archive/src/helpers/cloudinary.ts
@@ -18,7 +18,6 @@ import { v2 as cloudinary } from "cloudinary";
     process.env.CLOUDINARY_PRESET
   );
 
-<<<<<<< HEAD
   doPresetExists
     .catch((error: any) => {
       if (error.error.http_code === ERROR_CODES["NOT FOUND"]) {
@@ -41,25 +40,4 @@ export const uploadToCloudinary = (dataURI: string, filename: string) =>
     upload_preset: process.env.CLOUDINARY_PRESET,
     public_id: filename,
     resource_type: "auto",
-=======
-  doPresetExists.catch((error) => {
-    if (error.error.http_code === ERROR_CODES["NOT FOUND"]) {
-      cloudinary.api.create_upload_preset({
-        name: process.env.CLOUDINARY_PRESET,
-        folder: process.env.CLOUDINARY_FOLDER,
-        allowed_formats: ALLOWED_FILE_FORMATS.join(", "),
-        unsigned: true,
-        format: TRANSFORMED_FORMAT,
-        async: true,
-        notification_url: `${process.env.PRODUCTION_URL}${ApiRoutes.dashboard.uploadNotification}`,
-      });
-      console.log("New upload preset created");
-    } else console.error(error);
-  });
-})();
-
-export const uploadToCloudinary = (filepath: string) =>
-  cloudinary.uploader.upload(filepath, {
-    upload_preset: process.env.CLOUDINARY_PRESET,
->>>>>>> bea46711825ba60b9e1d7dfd9982d2107c641d72
   });
