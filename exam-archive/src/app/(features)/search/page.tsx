fe84cae7fad7e0edb28b3bc6c46f9f46719c19f3
bismay <bismaypurkayastha5467@gmail.com>
2024-02-03 16:37:35 +0530

    basic layout for search page

---
+++ exam-archive/src/app/(features)/search/page.tsx
@@ -0,0 +1,8 @@
+export default function Search() {
+  return (
+    <div>
+      This will be the search page. Change the content of the page according to
+      the requirements
+    </div>
+  );
+}
