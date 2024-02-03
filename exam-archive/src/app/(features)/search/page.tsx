4053d1541a5716e51a974c28379da7e52ec76a56
bismay <bismaypurkayastha5467@gmail.com>
2024-02-03 16:52:34 +0530

    changes made

--- exam-archive/src/app/(features)/search/page.tsx
+++ exam-archive/src/app/(features)/search/page.tsx
@@ -1,8 +1,27 @@
+"use client";
+
+import signOutAction from "@/actions/auth/signOut/POST/signOutAction";
+import { Button } from "@/components/ui/button";
+import { PageRoutes } from "@/constants/route";
+import { useRouter } from "next/navigation";
+
 export default function Search() {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    const res = await signOutAction();
+    if (res.hasError) return;
+    router.replace(PageRoutes.auth.signIn);
+  };
   return (
-    <div>
-      This will be the search page. Change the content of the page according to
-      the requirements
-    </div>
+    <>
+      <div>
+        This will be the search page. Change the content of the page according
+        to the requirements
+      </div>
+      <Button type="button" onClick={handleSignOut}>
+        SignOut
+      </Button>
+    </>
   );
 }
